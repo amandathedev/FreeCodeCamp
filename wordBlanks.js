adf9ab5d15375f400b3bb860346315cb77210d60
Amanda Treutler <amandathedev@gmail.com>
2019-05-07 22:57:43 -0500

    Formatting, examples

--- wordBlanks.js
+++ wordBlanks.js
@@ -1,11 +1,12 @@
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
     var result = "";
     // Your code below this line
-    result+= "My "+myAdjective+" "+myNoun+" "+myVerb+" very "+myAdverb+".";
+    result += "My " +myAdjective+ " "+myNoun+" "+myVerb+" very "+myAdverb+".";
 
     // Your code above this line
   return result;
 }
 
 // Change the words here to test your function
-wordBlanks("dog", "big", "ran", "quickly");
+// wordBlanks("dog", "big", "ran", "quickly");
+wordBlanks("cat", "small", "jumped", "high");
