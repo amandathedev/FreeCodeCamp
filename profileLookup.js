954c8dc78ee6c9ddd99dc424de7c34579a1e65bd
Amanda Treutler <amandathedev@gmail.com>
2019-05-12 20:05:51 -0500

    Profile lookup

---
+++ profileLookup.js
@@ -0,0 +1,10 @@
+for (var x = 0; x < contacts.length; x++){
+    if (contacts[x].firstName === name) {
+        if (contacts[x].hasOwnProperty(prop)) {
+            return contacts[x][prop];
+        } else {
+            return "No such property";
+        }
+    }
+}
+return "No such contact";
